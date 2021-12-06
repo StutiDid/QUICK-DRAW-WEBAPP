9ccbdb4c1c1326e4f75910782974d00177b8155f
StutiDid <77789710+StutiDid@users.noreply.github.com>
2021-12-06 21:23:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,3 +30,30 @@ function setup() {
    function clearCanvas() {
        background("white");
    }
+
+   function draw(){
+    strokeWeight(13);
+    stroke(0);
+
+    if (mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
+function classifycanvas(){
+    classifier.classify(canvas,gotResult)
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML='label :'+results[0].label;
+
+document.getElementById('confidence').innerHTML = 'Confidence :'+ Math.round(results[0].confidence *100) + '%';
+
+utterThis = new SpeechSynthesisUtterance(results[0].label);
+synth.speak(utterThis);
+
+}
+   
